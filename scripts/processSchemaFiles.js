6beec58d0149b975686ce0efa89ecd31e579d817
Teppo Kurki <teppo.kurki@iki.fi>
2021-10-31 17:17:56 +0200

    doc: switch to mdbook
    Fixes #569.

--- scripts/processSchemaFiles.js
+++ scripts/processSchemaFiles.js
@@ -344,8 +344,8 @@ class Parser {
       })
 
       fs.writeFileSync(path.join(__dirname, '../src/keyswithmetadata.json'), JSON.stringify(keysWithMeta, null, 2))
-      fs.writeFileSync(path.join(__dirname, '../gitbook-docs/vesselsBranch.md'), vesselsDoc)
-      fs.writeFileSync(path.join(__dirname, '../gitbook-docs/otherBranches.md'), othersDoc)
+      fs.writeFileSync(path.join(__dirname, '../mdbook/src/vesselsBranch.md'), vesselsDoc)
+      fs.writeFileSync(path.join(__dirname, '../mdbook/src/otherBranches.md'), othersDoc)
     })
 
     /*
